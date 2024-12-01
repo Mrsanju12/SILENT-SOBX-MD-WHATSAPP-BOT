3cf20ec7eacf3f08f66a412d66a7452a280e042b
jiraf102301 <160308312+jiraf102301@users.noreply.github.com>
2024-12-01 15:11:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "CrJn0BaY#PdEPvcrv-pUw6oJGqbaCc0gjc7mJ94YdHnmB7-3FDlo",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
